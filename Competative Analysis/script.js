afa0147ac8263e1205749d8afaaf703dd7765c3d
NancyDhakate <143214811+NancyDhakate@users.noreply.github.com>
2025-01-11 23:16:41 +0530

    popup

---
+++ Competative Analysis/script.js
@@ -0,0 +1,11 @@
+const popup = document.getElementById("popup");
+
+// Show popup
+function showPopup() {
+  popup.classList.add("active");
+}
+
+// Close popup
+function closePopup() {
+  popup.classList.remove("active");
+}
